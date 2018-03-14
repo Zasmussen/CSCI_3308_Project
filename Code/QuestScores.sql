f86b814459b0fa1c1d6af400ba3bc57c42c3acd2
Bekah <reha8209@colorado.edu>
2018-03-14 11:30:56 -0600

    Trying to clean up master

--- Code/QuestScores.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE QuestScores	;
- CREATE TABLE QuestScores
-   (
-   	userID	int 	NOT NULL,
-   	Score 	int 	NULL,
-   	PRIMARY KEY ( userID )  
-   )
-   CHARACTER SET utf8 COLLATE utf8_general_ci;
