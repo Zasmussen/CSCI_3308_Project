63c2ab132d1a72b28946cd636a353427e48082da
Bekah <reha8209@colorado.edu>
2018-03-16 11:32:05 -0600

    Put users into database-easier to start with users built in

--- Code/src/QuestUsers.sql
+++ Code/src/QuestUsers.sql
@@ -8,3 +8,10 @@ DROP TABLE   QuestUsers  ;
    PRIMARY KEY ( userID )  
    ) 
    CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+INSERT INTO QuestUsers VALUES (1, "Bekah", "reha8209@colorado.edu", "Bekah");
+INSERT INTO QuestUsers VALUES (2, "Jorge", "jorge.pulidolopez@colorado.edu", "Jorge");
+INSERT INTO QuestUsers VALUES (3, "Zach", "zachary.asmussen@colorado.edu", "Zach");
+INSERT INTO QuestUsers VALUES (4, "Haotian", "haotian.zheng@colorado.edu", "Haotian");
+INSERT INTO QuestUsers VALUES (5, "Theo", "theodore.margoles@colorado.edu", "Theo");
+INSERT INTO QuestUsers VALUES (6, "Binpeng", "binpeng.wu@colorado.edu", "Binpeng");
