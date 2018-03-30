9f6cf846f50983d6046579538b71269101049372
Bekah <reha8209@colorado.edu>
2018-03-30 09:33:02 -0600

    Updated QuestProducts and added temporary QuestScores table

--- Code/src/QuestScores.sql
+++ Code/src/QuestScores.sql
@@ -2,7 +2,14 @@ DROP TABLE QuestScores	;
  CREATE TABLE QuestScores
    (
    	userID	int 	NOT NULL,
-   	Score 	int 	NULL,
-   	PRIMARY KEY ( userID )  
+   	Score 	float(7,2) 	NULL,
+   	PRIMARY KEY ( userID )
    )
    CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+   INSERT INTO QuestScores VALUES (1, 123.4);
+   INSERT INTO QuestScores VALUES (2, 5.67);
+   INSERT INTO QuestScores VALUES (3, 89.0);
+   INSERT INTO QuestScores VALUES (4, 12.3);
+   INSERT INTO QuestScores VALUES (5, 45.6);
+   INSERT INTO QuestScores VALUES (6, 78.9);
