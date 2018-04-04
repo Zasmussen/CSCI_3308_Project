96c9a6abc99d739a96756f4e04753d32eb989dc1
Bekah <reha8209@colorado.edu>
2018-04-04 11:31:29 -0600

    Login page and submit work

--- Code/src/routes/index.js
+++ Code/src/routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express')
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+	host     : 'localhost',
+ 	user     : 'root',
+	password : '323013Rdh!',
+	database : 'CostQuest'
+});
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");
+} else {
+    console.log("Error connecting database ... nn");
+}
+});
 var app = express()
 app.get('/', function(req, res) {
- // render to views/index.ejs template file
- res.render('index', {title: 'Login Page'})
+ res.render('index', {title: 'CostQuest Login Page'})
  // Title is the custom title which you pass to be added in the header layout
 })
 /**
@@ -13,4 +26,16 @@ app.get('/', function(req, res) {
  * module.exports should be used to return the object
  * when this file is required in another module like app.js
  */
+
+app.get('/login', function(req,res) {
+	res.send('Welcome ' + req.query['username'] + '! Your current score is XXX.');
+// want to query QuestUsers database to make sure username and password exist.
+// If they do, want to res.send('Welcome ' req.query['username']) and say something about current score from QuestScore mysql table
+// If they don't exist, wnat to do res.send('invalid login creditials. try again') and return to login page
+
+
+})
+
+
+
 module.exports = app;
