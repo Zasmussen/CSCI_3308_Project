f9a319ed5731a60217579bf579c8c16b41aacd48
Theodore Margoles <thma4828@colorado.edu>
2018-04-18 15:03:42 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ var config = {
  host: 'us-cdbr-iron-east-05.cleardb.net', // database host
  user: 'b1ba0edd9924f7', // your database username
  password: 'd6a655f3', // your database password
- port: 5432, // port for heroku database
+ port: process.env.PORT, // port for heroku database
  db: 'heroku_bec06c68575ec5b' // your database name
  },
  server: {
  host: '198.105.244.23',
- port: 5432
+ port: process.env.PORT
  }
 }
 module.exports = config //Expose the current config as a module
