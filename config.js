bd4f9d06aeb50e6be7fb9ef1d35272c4c3dbf1dc
Theodore Margoles <thma4828@colorado.edu>
2018-04-18 14:37:44 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 var config = {
  database: {
- host: 'localhost', // database host
- user: 'root', // your database username
- password: 'rythmic8480', // your database password
- port: 3306, // default MySQL port
- db: 'costquest' // your database name
+ host: 'us-cdbr-iron-east-05.cleardb.net', // database host
+ user: 'b1ba0edd9924f7', // your database username
+ password: 'd6a655f3', // your database password
+ port: 5432, // port for heroku database
+ db: 'heroku_bec06c68575ec5b' // your database name
  },
  server: {
- host: '127.0.0.1',
- port: '4000'
+ host: '198.105.244.23',
+ port: 5432
  }
 }
 module.exports = config //Expose the current config as a module
