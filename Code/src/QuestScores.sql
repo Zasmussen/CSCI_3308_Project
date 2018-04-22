a8ed9862f0529a57558ee021b249d3b09cbb8af5
zasmussen <zaas3933@colorado.edu>
2018-04-22 12:36:03 -0600

    Changed scores to include games column

--- Code/src/QuestScores.sql
+++ Code/src/QuestScores.sql
@@ -1,15 +1,16 @@
 DROP TABLE QuestScores	;
  CREATE TABLE QuestScores
    (
-   	userID	int 	NOT NULL,
+   	userID	int 	NOT NULL AUTO_INCREMENT,
+	  games   int   NOT NULL,
    	Score 	float(7,2) 	NULL,
    	PRIMARY KEY ( userID )
    )
    CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-   INSERT INTO QuestScores VALUES (1, 123.4);
-   INSERT INTO QuestScores VALUES (2, 5.67);
-   INSERT INTO QuestScores VALUES (3, 89.0);
-   INSERT INTO QuestScores VALUES (4, 12.3);
-   INSERT INTO QuestScores VALUES (5, 45.6);
-   INSERT INTO QuestScores VALUES (6, 78.9);
+   INSERT INTO QuestScores VALUES (1, 0 ,0);
+   INSERT INTO QuestScores VALUES (2, 0, 0);
+   INSERT INTO QuestScores VALUES (3, 0, 0);
+   INSERT INTO QuestScores VALUES (4, 0, 0);
+   INSERT INTO QuestScores VALUES (5, 0, 0);
+   INSERT INTO QuestScores VALUES (6, 0, 0);
